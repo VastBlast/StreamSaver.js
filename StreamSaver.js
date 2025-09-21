a1e9a814445db7d7940f2db481b5b619bef5da91
VastBlast <48421698+VastBlast@users.noreply.github.com>
2025-09-21 16:47:18 -0400

    Revert "change to getStreamSaver"
    This reverts commit 990d38bff392e672605c784c109f73251374baed.
    
    revert

--- StreamSaver.js
+++ StreamSaver.js
@@ -8,7 +8,7 @@
     : typeof define === 'function' && typeof define.amd === 'object'
       ? define(definition)
       : this[name] = definition()
-})('getStreamSaver', () => {
+})('streamSaver', () => {
   'use strict'
 
   const global = typeof window === 'object' ? window : this
@@ -330,15 +330,7 @@
     }, opts.writableStrategy)
   }
 
-  return (opts = {}) => {
-    if (opts.mitm) {
-      streamSaver.mitm = opts.mitm
-    }
-    if (opts.WritableStream) {
-      streamSaver.WritableStream = opts.WritableStream
-    }
-    return streamSaver
-  }
+  return streamSaver
 });
 
 
